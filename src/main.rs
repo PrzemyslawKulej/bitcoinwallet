3f6511825c27581db155653abbc44b67e70c2f9d
pkulej <przemek.kulej1@gmail.com>
2024-04-03 23:36:09 +0200

    Added ? operator to error handle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use std::env;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, {}!", "World");
+    dotenv::from_filename(".env").ok();
+
+    let descriptor = env::var("WALLET_DESCRIPTOR")?;
+
+    println!("Descriptor: {}", descriptor);
+    dbg!(descriptor);
+
+    Ok(())
 }
